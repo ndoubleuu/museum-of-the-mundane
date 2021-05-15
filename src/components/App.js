dc3ba623a0f99d8fe856681f8faf689edbe04c11
Nicole <nicolewu.ncw@gmail.com>
2021-05-15 04:53:11 -0400

    slight styling

--- src/components/App.js
+++ src/components/App.js
@@ -56,19 +56,20 @@ function App() {
       <Header />
 
       <main>
+          <div className="wrapper">
 
-        <section>
-          <div className="description">
-              <p>From the smell of freshly brewed coffee in the morning, to the wildflowers blooming right outside your door...</p>
-              <p>There is beauty in everyday life.</p> 
-              <p>We’d love to hear about the little joyful things in your life. Add to our curated collection of everyday happy instances.</p>  
-          </div>
+          <section>
+            <div className="description">
+                <p>From the smell of freshly brewed coffee in the morning, to the wildflowers blooming right outside your door...</p>
+                <p>There is beauty in everyday life.</p> 
+                <p>We’d love to hear about the little joyful things in your life. Add to our curated collection of everyday happy instances.</p>  
+            </div>
 
-          <Form handleMoment={handleUserInput} />
-        </section>
+            <Form handleMoment={handleUserInput} />
+          </section>
 
-        <Gallery momentsData={moments} />
-
+          <Gallery momentsData={moments} />
+        </div>
       </main>
 
       <footer>
