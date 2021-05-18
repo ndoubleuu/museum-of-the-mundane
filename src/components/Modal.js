3db83c07ae845c8942a045454a4283bf213056b3
Nicole <nicolewu.ncw@gmail.com>
2021-05-18 00:22:32 -0400

    cleaned up code, less lines, but still doing the do

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,14 +7,14 @@ const Modal = (props) => {
     const { showModal, exitModal, removePost, itemSelected } = props;
 
     return (
-        <div className={showModal === true ? "show-modal" : "hide-modal"}>
+        <div className={showModal ? null : "hide-modal"}>
             <div className="modal">
                 <div className="modal-content">
                     <button className="exit-modal" onClick={exitModal}>
                         <span className="sr-only">Exit modal</span>
                         <FontAwesomeIcon icon={faTimesCircle} aria-hidden="true" />
                     </button>
-                    <p>Are you sure that you'd like to remove this post? This action cannot be undone.</p>
+                    <p>Are you sure you'd like to remove this post? This action cannot be undone.</p>
                     <button className="confirm-delete" 
                         onClick={ () => {
                             removePost(itemSelected);
